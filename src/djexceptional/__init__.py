5250c9b6ad35d76ac4f2e6fc14c2a7765cd1713a
Zachary Voase <z@zacharyvoase.com>
2011-09-08 01:13:58 +0000

    Bumped to v0.1.4.

--- src/djexceptional/__init__.py
+++ src/djexceptional/__init__.py
@@ -17,7 +17,7 @@ from django.core.urlresolvers import resolve
 from djexceptional.utils import memoize, json_dumps, meta_to_http
 
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 EXCEPTIONAL_PROTOCOL_VERSION = 6
 EXCEPTIONAL_API_ENDPOINT = getattr(settings, 'EXCEPTIONAL_API_ENDPOINT',
